1dec81e85a06dc25c076ad209b3d9435ca225f19
WinterR Frost <frostywinterr@gmail.com>
2018-09-09 22:04:16 +0200

    Update init_sec.cpp

--- init/init_sec.cpp
+++ init/init_sec.cpp
@@ -42,6 +42,8 @@ void vendor_load_properties()
 	
     std::string model;
     std::string name;
+    std::string description;
+    std::string fingerprint;
 
     model = "SM-" + bl_model;
 
@@ -53,20 +55,25 @@ void vendor_load_properties()
 
     if (name.size() == 0) {
         LOG(ERROR) << "Could not detect codename, forcing j7xeltexx";
-        name = "j7xeltedd";
+        name = "j7xeltexx";
     }
-
+	
+    description = "j7xeltexx-user 7.0 NRD90M J710FXXU3BQJ3 release-keys";
+    fingerprint = "samsung/j7xeltexx/j7xelte:7.0/NRD90M/J710FXXU3BQJ3:user/release-keys";
 
     LOG(INFO) << "Found bootloader: %s", bootloader.c_str();
     LOG(INFO) << "Setting ro.product.model: %s", model.c_str();
     LOG(INFO) << "Setting ro.product.device: %s", device.c_str();
     LOG(INFO) << "Setting ro.product.name: %s", name.c_str();
     LOG(INFO) << "Setting ro.build.product: %s", device.c_str();
+    LOG(INFO) << "Setting ro.build.description: %s", description.c_str();
+    LOG(INFO) << "Setting ro.build.fingerprint: %s", fingerprint.c_str();
 
     property_override_dual("ro.product.model", "ro.vendor.product.model", model.c_str());
     property_override_dual("ro.product.device", "ro.vendor.product.device", device.c_str());
     property_override_dual("ro.product.name", "ro.vendor.product.name", name.c_str());
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", fingerprint.c_str());
 
     property_override("ro.build.product", device.c_str());
-
+    property_override("ro.build.description", description.c_str());
 }
